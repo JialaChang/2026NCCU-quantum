3c8882790cf973d448b98374f13af3920fa26766
JialaChang <jiala20061218@gmail.com>
2026-05-02 15:17:35 +0800

    add auto find path

--- qubit.py
+++ qubit.py
@@ -1,7 +1,7 @@
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, ClassicalRegister
 from qiskit_aer import AerSimulator
-from qiskit import ClassicalRegister
 import matplotlib.pyplot as plt
+from collections import deque
 
 def create_circuit(num: int, path: list[int]):
     """
@@ -55,19 +55,55 @@ def create_ladder_node(L: int):
     return graph
 
 
+def find_best_path(graph, start, end):
+    """
+    use BFS algorithm to find path
+    """
+    queue = deque([start])
+    backward = {start: None}
+    while queue:
+        # get the first of queue
+        curr = queue.popleft()
+
+        # find end return path
+        if curr == end:
+            path = []
+            # backward the path
+            while curr is not None:
+                path.append(curr)
+                curr = backward[curr]
+            # reserve the path
+            return path[::-1]
+        
+        # traverse node's nieghbor
+        for nieghbor in graph[curr]:
+            # dont go back
+            if nieghbor not in backward:
+                backward[nieghbor] = curr
+                queue.append(nieghbor)
+    
+    return []
+
+
 if __name__ == "__main__":
 
+    # generate path of circuit
     L = 5
     num_qubit = 2 * L + 2
-    circuit_path = [0, 1, 7, 8, 9, 3, 4, 5]
-    final_qc = create_circuit(num_qubit, circuit_path)
+    ladder_graph = create_ladder_node(L)
+    start_node = 0
+    end_node = L
+    dynamic_path = find_best_path(ladder_graph, start_node, end_node)
+    print(f"plan path: {dynamic_path}")
+
+    final_qc = create_circuit(num_qubit, dynamic_path)
     # measure qubit
     cr = ClassicalRegister(2, 'meas')
     final_qc.add_register(cr)
-    final_qc.measure(circuit_path[0], 0)
-    final_qc.measure(circuit_path[-1], 1)
+    final_qc.measure(dynamic_path[0], 0)
+    final_qc.measure(dynamic_path[-1], 1)
 
-    # quantum simulator 
+    # quantum simulator
     simulator = AerSimulator()
     job = simulator.run(final_qc, shots=1024)  # run n times
     result = job.result()
